5a69ea2567fd52c945536de2fcf487f5a78292f3
Antony - Kanas <kanashikia26@gmail.com>
2023-03-30 23:08:46 +0200

    feat(ingredient-modal): add feature

--- components/AppAlert.vue
+++ components/AppAlert.vue
@@ -5,19 +5,49 @@
     v-for="(o, index) in beers"
     :key="o" 
     :body-style="{ padding: '0px' }">
-    <img
-      :src="o.image_url"
-      class="image"
-    />
-    <div style="padding: 14px">
-      <h1>{{o.name}}</h1>
-      <div class="bottom">
-        <div class="description">{{ o.description }}</div>
-        <div class="first-brewed">First brewed : {{ o.first_brewed }}</div>
-        <el-button type="primary" text class="button">Ingredients</el-button>
+      <img
+        :src="o.image_url"
+        class="image"
+      />
+      <div style="padding: 14px">
+        <h1>{{o.name}}</h1>
+        <div class="bottom">
+          <el-scrollbar height="10vh">
+            <div class="description">{{ o.description }}</div>
+          </el-scrollbar>
+          
+          <div class="first-brewed">First brewed : {{ o.first_brewed }}</div>
+          <el-button type="primary" text class="button" @click=" openDialog(o)">Ingredients</el-button>
+        </div>
       </div>
-    </div>
-  </el-card>
+    </el-card>
+
+    <el-dialog
+        v-model="centerDialogVisible"
+        title="Ingredients"
+        width="50vw"
+        align-center
+        draggable
+
+      >
+          <div class="dialog-footer">
+            <h2 v-if="clickedBeer.ingredients.malt">Malt</h2>
+            <div v-for="ingredient in clickedBeer.ingredients.malt" class="malt">
+              {{ ingredient.name }}
+              {{ ingredient.amount.value }}
+              {{ ingredient.amount.unit }}
+            </div>
+            <h2 v-if="clickedBeer.ingredients.hop">Hop</h2>
+            <div v-for="ingredient in clickedBeer.ingredients.hop" class="hop">
+              {{ ingredient.name }}
+              {{ ingredient.amount.value }}
+              {{ ingredient.amount.unit }}
+            </div>
+            <h2 v-if="clickedBeer.ingredients.yeast">Yeast</h2>
+            <div>{{ clickedBeer.ingredients.yeast }}</div>
+            
+          </div>
+      </el-dialog>
   </div>
   
 </template>
@@ -25,7 +55,14 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 const beers = await fetch('https://api.punkapi.com/v2/beers').then(res => res.json())
-const currentDate = ref(new Date())
+const centerDialogVisible = ref(false)
+let clickedBeer: any;
+
+const openDialog = (beer: any) => {
+  clickedBeer = beer
+  centerDialogVisible.value = true
+}
+
 </script>
 
 <style>
